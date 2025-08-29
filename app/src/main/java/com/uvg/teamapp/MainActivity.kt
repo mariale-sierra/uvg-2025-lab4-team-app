be89f738fe310b2f7ae1d4467402c1e5d50473c4
camsandvl <san24358@uvg.edu.gt>
2025-08-29 09:12:42 -0600

    cree la lista, y modifique main

--- app/src/main/java/com/uvg/teamapp/MainActivity.kt
+++ app/src/main/java/com/uvg/teamapp/MainActivity.kt
@@ -7,10 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.uvg.teamapp.repositorio.NewTeamRepositorio
 import com.uvg.teamapp.ui.theme.TeamAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +19,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TeamAppTheme {
+                // Obtenemos la lista de miembros desde el FakeTeamRepository
+                val team = NewTeamRepositorio.FakeTeamRepository.getTeamMembers()
+
+                // Mostramos tu pantalla con LazyColumn
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Equipo",
+                    TeamListScreen(
+                        teamMembers = team,
+                        // Para respetar el padding que te da el Scaffold
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hola $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TeamAppTheme {
-        Greeting("Equipo")
     }
 }
